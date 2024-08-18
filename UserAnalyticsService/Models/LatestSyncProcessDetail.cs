25d4f3d969b28506d3670d9185635c7d8590c436
Aritra Nayak <aritra.dhrub@gmail.com>
2024-08-18 22:17:26 +0530

    LatestSyncProcessDetail Ongoing

---
+++ UserAnalyticsService/Models/LatestSyncProcessDetail.cs
@@ -0,0 +1,14 @@
+﻿namespace UserAnalyticsService.Models
+{
+    public class LatestSyncProcessDetail
+    {
+        public SyncProcessTypeEnum SyncProcessType { get; set; }
+        public DateTime ProcessUpdateTillDate { get; set; }
+    }
+
+    public enum SyncProcessTypeEnum
+    {
+        Security = 1,
+        Price = 2
+    }
+}
---
+++ UserAnalyticsService/Repository/LatestSyncProcessDetailRepository.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Driver;
+using UserAnalyticsService.Models;
+using UserAnalyticsService.Repository.IRepository;
+
+namespace UserAnalyticsService.Repository
+{
+    public class LatestSyncProcessDetailRepository : GenericRepository<LatestSyncProcessDetail>, ILatestSyncProcessDetailRepository
+    {
+        private readonly IMongoDatabase _database;
+
+        public LatestSyncProcessDetailRepository(IMongoDatabase database)
+            : base(database, "LatestSyncProcessDetails") // "LatestSyncProcessDetails" is the collection name in MongoDB
+        {
+            _database = database;
+        }
+
+        public IMongoCollection<LatestSyncProcessDetail> LatestSyncProcessDetails => _collection;
+    }
+}
